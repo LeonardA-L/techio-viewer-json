7a077a247efffdfc86a129f32f2e66d8ed337646
Leonard Allain-Launay <leonard@codingame.com>
2017-06-14 22:34:53 -0700

    Add json viewer files

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -R viewer-json /tmp/
+
+cp techio-viewer-json.sh /usr/bin/techio-viewer-json
+chmod +x /usr/bin/techio-viewer-json
---
+++ techio-viewer-json.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv $1 /tmp/viewer-json/file.json
+
+echo 'CG> open --static-dir /tmp/viewer-json /json.html'
